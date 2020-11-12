415bf5bf755edf39ccb786890368be0d4154a766
Wesley Dusell <wesleydusell@Wesleys-MacBook-Air.local>
2020-11-12 16:24:22 -0500

    Create component, service and, pipe module for the shared module.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentsModule } from './components/components.module';
+import { ServicesModule } from './services/services.module';
+import { PipesModule } from './pipes/pipes.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule,
+    ServicesModule,
+    PipesModule
   ]
 })
 export class SharedModule { }
