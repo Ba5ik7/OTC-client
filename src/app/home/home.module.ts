d210ad863571df48628051a2f019cf67f4d33ca9
Wesley Dusell <wesleydusell@Wesleys-MacBook-Air.local>
2020-11-12 15:47:46 -0500

    Setup home feature module to lazy load and to have it's own routing. Created a Home 'it works' component.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { HomeRoutingModule } from './home-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
