e2f90c677d404727e61abda2fa228e2ec86004a8
Wesley Dusell <wesleydusell@Wesleys-MacBook-Air.local>
2020-11-12 15:39:05 -0500

    Create the apps feature modules and import them.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
+import { HomeModule } from './home/home.module';
+import { EventsModule } from './events/events.module';
+import { ThingsToDoModule } from './things-to-do/things-to-do.module';
+import { NewsModule } from './news/news.module';
+import { GetInvolvedModule } from './get-involved/get-involved.module';
+import { AboutOtcModule } from './about-otc/about-otc.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { MaterialModule } from './material-module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HomeModule,
+    EventsModule,
+    ThingsToDoModule,
+    NewsModule,
+    GetInvolvedModule,
+    AboutOtcModule
   ],
   providers: [],
   bootstrap: [AppComponent]
